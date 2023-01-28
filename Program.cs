3b1f7af9a84b3bdcd25f343b6cb9a621328944e1
Кирилл Зыков <zykovkirill95@mail.ru>
2023-01-28 19:19:25 +0300

    [+] init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Grpc.Net.Client;
+using BookStorageGrpcClient;
+
+// создаем канал для обмена сообщениями с сервером
+// параметр - адрес сервера gRPC
+using var channel = GrpcChannel.ForAddress("http://localhost:5098");
+// создаем клиент
+var client = new BookStorage.BookStorageClient(channel);
+Console.Write("Введите автора: ");
+string? author = Console.ReadLine();
+// обмениваемся сообщениями с сервером
+var reply = await client.LoadAllBooksAsync(new LoadRequest { Author = author });
+Console.WriteLine($"Ответ сервера: {reply.Books}");
+Console.ReadKey();
